696a5b0d56c8082e6e3d0c41ce983819e2fb2807
LeeKiHwan <106875970+LeeKiHwan@users.noreply.github.com>
2022-06-16 13:53:22 +0900

    Update Random.c

--- Random.c
+++ Random.c
@@ -1,143 +1,228 @@
+#include <windows.h>
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int gamecom(int student) {
-	switch (student)
-	{
-	case 0:
+int gameStudent=3, securityStudent=0, itStudent=0, random;
+
+void gotoxy(int x, int y);
+void LoadingCom();
+
+void gamecom() {
+	random = rand() % 100 + 1;
+	if (gameStudent == 0) {
+		gotoxy(20, 18);
 		printf("인원이 부족합니다.");
-		break;
+		return 0;
+	}
+	gotoxy(20, 18);
+	printf("                    ");
+	LoadingCom();
+	gotoxy(20, 18);
+	printf("                    ");
+	gotoxy(20, 18);
+	switch (gameStudent)
+	{
 	case 1:
-		student = rand() % 100 + 1;
-		if (student <= 20) {
+		if (random <= 20) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		gameStudent -= 1;
 		break;
 	case 2:
-		student = rand() % 100 + 1;
-		if (student <= 40) {
+		if (random <= 40) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		gameStudent -= 2;
 		break;
 	case 3:
-		student = rand() % 100 + 1;
-		if (student <= 66) {
+		if (random <= 66) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		gameStudent -= 3;
 		break;
 
 	default:
 		break;
 	}
+	featurescreen();
 }
 
-int securitycom(int student) {
-	switch (student)
-	{
-	case 0:
+void securitycom() {
+	random = rand() % 100 + 1;
+	if (securityStudent == 0) {
+		gotoxy(20, 18);
 		printf("인원이 부족합니다.");
-		break;
+		return 0;
+	}
+	gotoxy(20, 18);
+	printf("                    ");
+	LoadingCom();
+	gotoxy(20, 18);
+	printf("                    ");
+	gotoxy(20, 18);
+	switch (securityStudent)
+	{
 	case 1:
-		student = rand() % 100 + 1;
-		if (student <= 25) {
+		if (random <= 25) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		securityStudent -= 1;
 		break;
 	case 2:
-		student = rand() % 100 + 1;
-		if (student <= 55) {
+		if (random <= 55) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		securityStudent -= 2;
 		break;
 	case 3:
-		student = rand() % 100 + 1;
-		if (student <= 80) {
+		if (random <= 80) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		securityStudent -= 3;
 		break;
 
 	default:
 		break;
 	}
+	featurescreen();
 }
 
-int itcom(int student) {
-	switch (student)
-	{
-	case 0:
+void itcom() {
+	random = rand() % 100 + 1;
+	if (itStudent == 0) {
+		gotoxy(20, 18);
 		printf("인원이 부족합니다.");
-		break;
+		return 0;
+	}
+	gotoxy(20, 18);
+	printf("                    ");
+	LoadingCom();
+	gotoxy(20, 18);
+	printf("                    ");
+	gotoxy(20, 18);
+	switch (itStudent)
+	{
 	case 1:
-		student = rand() % 100 + 1;
-		if (student <= 33) {
+		if (random <= 33) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		itStudent -= 1;
 		break;
 	case 2:
-		student = rand() % 100 + 1;
-		if (student <= 66) {
+		if (itStudent <= 66) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		itStudent -= 2;
 		break;
 	case 3:
-		student = rand() % 100 + 1;
-		if (student <= 99) {
+		if (itStudent <= 99) {
 			printf("success");
 		}
 		else
 			printf("fail");
+		itStudent -= 3;
 		break;
 
 	default:
 		break;
 	}
+	featurescreen();
+}
+
+int featurescreen() {
+	gotoxy(30, 0);
+	printf("□□□□□□□□□□□□□□□□□□□□□□□□□");
+	gotoxy(30, 1);
+	printf("□                   기능대회                   □");
+	gotoxy(30, 2);
+	printf("□□□□□□□□□□□□□□□□□□□□□□□□□");
+	gotoxy(0, 0);
+	printf("내 기능반 학생 수");
+	gotoxy(0, 1);
+	printf("▶ 게임 개발 : %d", gameStudent);
+	gotoxy(0, 2);
+	printf("▶ 사이버 보안 : %d", securityStudent);
+	gotoxy(0, 3);
+	printf("▶ IT 네트워크 : %d", itStudent);
+	gotoxy(20, 5);
+	printf("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+	gotoxy(20, 6);
+	printf("□                                                                  □");
+	gotoxy(20, 7);
+	printf("□     게임 개발 대회 나가기( T )                                   □");
+	gotoxy(20, 8);
+	printf("□     (확률 : 1명 20%%, 2명 40%%, 3명 66%%)                           □");
+	gotoxy(20, 9);
+	printf("□                                                                  □");
+	gotoxy(20, 10);
+	printf("□     사이버 보안 대회 나가기 ( Y )                                □");
+	gotoxy(20, 11);
+	printf("□     (확률 : 1명 25%%, 2명 55%%, 3명 80%%)                           □");
+	gotoxy(20, 12);
+	printf("□                                                                  □");
+	gotoxy(20, 13);
+	printf("□     IT 네트워크(<--우웩) 대회 나가기 ( U )                       □");
+	gotoxy(20, 14);
+	printf("□     (확률 : 1명 33%%, 2명 66%%, 3명 99%%)                           □");
+	gotoxy(20, 15);
+	printf("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+	gotoxy(20, 17);
 }
 
 int main() {
 	srand((unsigned)time(NULL));
 
-	int game, security, it, choose;
+	featurescreen();
 
-	printf("겜개 기능대회 인원 >> ");
-	scanf("%d", &game);
-	printf("보안 기능대회 인원>> ");
-	scanf("%d", &security);
-	printf("IT 기능대회 인원 >> ");
-	scanf("%d", &it);
+	char InputKey = "";
+	do {
+		
+		InputKey = _getch();
+		if (InputKey == 't') {
+			gamecom(gameStudent);
+		}
+		else if (InputKey == 'y') {
+			securitycom(securityStudent);
+		}
+		else if (InputKey == 'u') {
+			itcom(itStudent);
+		}
+		
+	} while (InputKey != 'q');
+}
 
-	printf("어느 대회에 나가시겠습니까? (겜개 : 1  보안 : 2  IT : 3)\n>>");
-	scanf("%d", &choose);
+void LoadingCom() {
+	gotoxy(20, 18);
+	printf("기능 대회 진행중");
 
-	switch (choose)
-	{
-	case 1:
-		gamecom(game);
-		break;
-	case 2:
-		securitycom(security);
-		break;
-	case 3:
-		itcom(it);
-		break;
-	default:
-		break;
-	}
+	Sleep(1000);
+	printf(".");
+	Sleep(1000);
+	printf(".");
+	Sleep(1000);
+	printf(".");
+}
 
+void gotoxy(int x, int y)
+{
+	COORD pos = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
